65b97b257c583241b3efcb19f23bc624b144bd85
emanuelgimenez2 <emanuelgimenez2@gmail.com>
2022-09-15 19:43:36 -0300

    add cambios

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -3,7 +3,7 @@ export default function SocialMedia() {
   return (
     <section className="icon-list" style={{display:'flex', gap:14, margin:14, zIndex: -1, position: 'absolute'}}>
     <a href="https://twitter.com/manurausc" target="_blank"><i className="nes-icon twitter is-medium"/></a>
-    {/* <a href="https://www.instagram.com/inversatil" target="_blank"><i className="nes-icon instagram is-medium"/></a> */}
+    {/* <a href="" target="_blank"><i className="nes-icon instagram is-medium"/></a> */}
     <a href="https://github.com/emanuelgimenez2" target="_blank"><i className="nes-icon github is-medium"/></a>
     <a href="https://www.linkedin.com/in/fiamma-muscari/" target="_blank"><i className="nes-icon linkedin is-medium"/></a>
       </section>
